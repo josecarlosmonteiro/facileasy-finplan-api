4a5397900ffc2aa6c5aea5f7d2cce9c6f5423dd9
Carlos Monteiro <j.de.araujo.monteiro@avanade.com>
2023-11-24 11:34:49 -0300

    feat: implementar primeiro server + middleware de acompanhamento de logs.

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,11 @@ import express from 'express';
 const app = express();
 const PORT: number = 4000;
 
-app.get('/', (req, res) => res.json({ message: 'Server changed!' }));
+// middleware para acompanhar logs de requisições
+app.use('/api', (req, res, next) => {
+  console.log(`[${req.method}] ${req.path}`);
+  next();
+});
+
+app.get('/', (req, res) => res.json({ message: 'Server running!' }));
 app.listen(PORT, () => console.log(`Server running in http://localhost:${PORT}`));
